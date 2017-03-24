0c170a9d2fd27e2c5c84d1b0ad72e7f3ced69b0b
jgrilli <jgrilli@pd.infn.it>
2017-03-24 13:07:05 -0500

    added pubs

--- styles/index.js
+++ styles/index.js
@@ -1,49 +1,90 @@
 var hide_lists = function(cb) {
-    $('#posts').fadeOut(300);
+    $('#posts').fadeOut(300); 
     $('#projects').fadeOut(300);
     $('#aboutme').fadeOut(300);
+    $('#publications').fadeOut(300);
     $('#posts-btn').removeClass('disabled');
-    $('#projects-btn').removeClass('disabled')
-    $('#aboutme-btn').removeClass('disabled')
+    $('#projects-btn').removeClass('disabled');
+    $('#aboutme-btn').removeClass('disabled');
+    $('#publications-btn').removeClass('disabled');
 };
-var show_projects = function() {
-//    $('#posts-btn').removeClass('disabled');
-//    $('#aboutme-btn').removeClass('disabled');
 
-//    $('#posts').fadeOut(300, function() {
-//        $('#projects').fadeIn(300)
-//    });
-//    $('#aboutme').fadeOut(300, function() {
-//        $('#projects').fadeIn(300)
-//    });
-    hide_lists()
-    $('#projects').fadeIn(0)
+var show_projects = function() {
+
+    if( $('#posts').is(':visible') ){
+        $('#posts-btn').removeClass('disabled');
+        $('#posts').fadeOut(300, function() { $('#projects').fadeIn(300); });
+    } else if( $('#publications').is(':visible') ){
+        $('#publications-btn').removeClass('disabled');
+        $('#publications').fadeOut(300, function() { $('#projects').fadeIn(300); });
+    } else if( $('#aboutme').is(':visible') ){
+        $('#aboutme-btn').removeClass('disabled');
+        $('#aboutme').fadeOut(300, function() { $('#projects').fadeIn(300); });
+    } else{
+        $('#projects').fadeIn(300);
+    }
     $('#projects-btn').addClass('disabled')
+
 };
 
 var show_posts = function() {
-//    $('#projects-btn').removeClass('disabled');
-//    $('#aboutme-btn').removeClass('disabled');
-//    $('#projects').fadeOut(function() {
-//        $('#posts').fadeIn(300)
-//    });
-//    $('#aboutme').fadeOut(function() {
-//        $('#posts').fadeIn(300)
-//    });
-    hide_lists()
-    $('#posts').fadeIn(0)
+
+    if( $('#projects').is(':visible') ){
+        $('#projects-btn').removeClass('disabled');
+        $('#projects').fadeOut(300, function() { $('#posts').fadeIn(300); });
+    } else if( $('#publications').is(':visible') ){
+        $('#publications-btn').removeClass('disabled');
+        $('#publications').fadeOut(300, function() { $('#posts').fadeIn(300); });
+    } else if( $('#aboutme').is(':visible') ){
+        $('#aboutme-btn').removeClass('disabled');
+        $('#aboutme').fadeOut(300, function() { $('#posts').fadeIn(300); });
+    } else{
+        $('#posts').fadeIn(300);
+    }
+
     $('#posts-btn').addClass('disabled')
+
 };
+
+
+
 var show_aboutme = function() {
-//    $('#posts-btn').removeClass('disabled');
-//    $('#projects-btn').removeClass('disabled');
-//    $('#posts').fadeOut(300, function() {
-//        $('#aboutme').fadeIn(300)
-//    });
-//    $('#projects').fadeOut(300, function() {
-//        $('#aboutme').fadeIn(300)
-//    });
-    hide_lists()
-    $('#aboutme').fadeIn(0)
+
+    if( $('#projects').is(':visible') ){
+        $('#projects-btn').removeClass('disabled');
+        $('#projects').fadeOut(300, function() { $('#aboutme').fadeIn(300); });
+    } else if( $('#publications').is(':visible') ){
+        $('#publications-btn').removeClass('disabled');
+        $('#publications').fadeOut(300, function() { $('#aboutme').fadeIn(300); });
+    }  else if( $('#posts').is(':visible') ){
+        $('#posts-btn').removeClass('disabled');
+        $('#posts').fadeOut(300, function() { $('#aboutme').fadeIn(300); });
+    } else{
+        $('#aboutme').fadeIn(300);
+    }
+
     $('#aboutme-btn').addClass('disabled')
+
 };
+
+var show_publications = function() {
+
+    if( $('#projects').is(':visible') ){
+        $('#projects-btn').removeClass('disabled');
+        $('#projects').fadeOut(300, function() { $('#publications').fadeIn(300); });
+    } else if( $('#aboutme').is(':visible') ){
+        $('#aboutme-btn').removeClass('disabled');
+        $('#aboutme').fadeOut(300, function() { $('#publications').fadeIn(300); });
+    }  else if( $('#posts').is(':visible') ){
+        $('#posts-btn').removeClass('disabled');
+        $('#posts').fadeOut(300, function() { $('#publications').fadeIn(300); });
+    } else{
+        $('#publications').fadeIn(300);
+    }
+
+    $('#publications-btn').addClass('disabled')
+
+};
+
+
+
