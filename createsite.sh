866bddaa6ae430a9d581fcc4af27f653aa300767
jacopogrilli <grilli.jacopo@gmail.com>
2018-07-20 10:11:11 -0600

    new pubs

--- createsite.sh
+++ createsite.sh
@@ -2,10 +2,22 @@
 ( cd ../CollabNet/ ; bash executeall.sh )
 
 # serve
+
+
 if [ "$1" == "-s" ]; then
+
+  for p in `ps aux | grep jekyll | awk '{print $2}'`; do
+      kill $p
+  done
+
   bundle exec jekyll build
-  bundle exec jekyll serve
+  xterm -hold -e bundle exec jekyll serve &
+  sleep 3s
+  google-chrome http://127.0.0.1:4000/ &
 fi
+
+
+
 
 if [ "$1" == "-p" ]; then
   git add .
